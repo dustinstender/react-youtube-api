2aaf6462e9c632a02bce63018d6dcb1e48de184e
Dustin Stender <dustinpstender@gmail.com>
2022-01-29 17:08:06 +0100

    get request gives 200, didnt add www....

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 const KEY = 'AIzaSyDfoVI_FaqHgMpugMgBhsrgRvDNqq5nfZM';
 
 export default axios.create({
-	baseURL: 'https://apis.google.com/youtube/v3',
+	baseURL: 'https://www.googleapis.com/youtube/v3',
 	params: {
 		part: 'snippet',
+		type: 'video',
 		maxResults: 5,
 		key: KEY,
 	},
